4d2daa2ddab0527306a98a6b1b8fa04f03fa32a3
amine louhidi <a.louhidi@hotmail.fr>
2014-02-14 10:59:33 +0100

    Configuration

--- src/com/mmm/findtherythm/IndexActivity.java
+++ src/com/mmm/findtherythm/IndexActivity.java
@@ -3,6 +3,7 @@ package com.mmm.findtherythm;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class IndexActivity extends Activity {
 
@@ -10,6 +11,9 @@ public class IndexActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_index);
+		TextView tv = new TextView(this);
+        tv.setText("Hello, Android oui Android");
+		setContentView(tv);
 	}
 
 	@Override
