616e59fd07014d76f41c400ad191095d84867365
medi24 <mehdi-but@hotmail.fr>
2014-02-16 17:02:02 +0100

    gestion du son

--- src/com/mmm/findtherythm/GameActivity.java
+++ src/com/mmm/findtherythm/GameActivity.java
@@ -1,15 +1,79 @@
 package com.mmm.findtherythm;
 
-import android.os.Bundle;
+
 import android.app.Activity;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
 
 public class GameActivity extends Activity {
-
+	//private Timer myTimer;
+	MediaPlayer mMediaPlayer = new MediaPlayer();
+	MediaPlayer mMediaPlayer2 = new MediaPlayer();
+	MediaPlayer mMediaPlayer3 = new MediaPlayer();
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_game);
+		Button btn = (Button) findViewById(R.id.button1);
+		Button btn2 = (Button) findViewById(R.id.button2);
+		
+		configSound();
+		mMediaPlayer2.start();
+    	oppaDance();
+        
+		btn.setOnClickListener(buttonHandler);
+		btn2.setOnClickListener(buttonHandler2);
+		}
+		
+	OnClickListener buttonHandler = new OnClickListener() {
+		@Override
+        public void onClick(View v) {
+			mMediaPlayer.start();  	
+        }
+	};
+	OnClickListener buttonHandler2 = new OnClickListener() {
+		@Override
+        public void onClick(View v) {
+			mMediaPlayer3.start();  	
+        }
+	};
+	
+
+	public void oppaDance(){
+		final RelativeLayout rl = (RelativeLayout) findViewById(R.id.layoutGameid);
+        rl.postDelayed(new Runnable() {
+            int i = 0;
+            public void run() {
+            	rl.setBackgroundResource(
+                    i++ % 2 == 0 ?
+                    		R.drawable.op2 :
+                    		R.drawable.op1);
+            	rl.postDelayed(this, 500);
+            }
+        }, 500);
+	}
+	public void configSound(){
+		mMediaPlayer = MediaPlayer.create(this, R.raw.error);
+		mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+		mMediaPlayer.setLooping(false);
+		mMediaPlayer.setVolume(100, 100);
+		mMediaPlayer3 = MediaPlayer.create(this, R.raw.valid);
+		mMediaPlayer3.setAudioStreamType(AudioManager.STREAM_MUSIC);
+		mMediaPlayer3.setLooping(false);
+		mMediaPlayer3.setVolume(100, 100);
+		mMediaPlayer2 = MediaPlayer.create(this, R.raw.sound1);
+		mMediaPlayer2.setAudioStreamType(AudioManager.STREAM_MUSIC);
+		mMediaPlayer2.setLooping(true);
+		mMediaPlayer.setVolume(20, 20);
 	}
 
 	@Override
--- src/com/mmm/findtherythm/IndexActivity.java
+++ src/com/mmm/findtherythm/IndexActivity.java
@@ -1,7 +1,14 @@
 package com.mmm.findtherythm;
 
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -9,29 +16,58 @@ import android.view.View.OnClickListener;
 
 
 public class IndexActivity extends Activity {
+	
+	MediaPlayer mMediaPlayer = new MediaPlayer();
+	MediaPlayer mMediaPlayer2 = new MediaPlayer();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_index);
-
+	
 
 		// Component declaration
 		Button buttonJouer = (Button) findViewById(R.id.buttonJouer);
 		Button buttonScore = (Button) findViewById(R.id.buttonScore);
 
+		mMediaPlayer2 = MediaPlayer.create(this, R.raw.sound1);
+		mMediaPlayer2.setAudioStreamType(AudioManager.STREAM_MUSIC);
+		mMediaPlayer2.setLooping(true);	
+		Button btn = (Button) findViewById(R.id.button1);
+		
 		// Handler declaration
 		buttonJouer.setOnClickListener(buttonJouerHandler);
 		buttonScore.setOnClickListener(buttonScoreHandler);
+	    btn.setOnClickListener(buttonHandler); 
+		
+		
 
 
 	}
+	
+	OnClickListener buttonHandler = new OnClickListener() {
+		@Override
+        public void onClick(View v) {
+        	//RelativeLayout rl = (RelativeLayout) findViewById(R.id.relativeLayoutid);
+        	//rl.setBackgroundResource(R.drawable.op2);
+        	 /*  Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+               Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), notification);
+               r.play();*/
+			mMediaPlayer.start();
+			
+        	
+        }
+	};
 
 	OnClickListener buttonJouerHandler = new OnClickListener() {
 
 		@Override
 		public void onClick(View v) {
-			setContentView(R.layout.activity_game);	
+			//setContentView(R.layout.activity_game);	
+			Intent intent = new Intent(IndexActivity.this, GameActivity.class);
+			startActivity(intent);
+			
+			//mMediaPlayer2.start();
 		}
 
 	};
@@ -45,6 +81,12 @@ public class IndexActivity extends Activity {
 		}
 
 	};
+	
+
+
+
+	
+		
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
